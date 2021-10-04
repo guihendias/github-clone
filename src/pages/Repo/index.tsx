57da4bbb7f49d2aee0fee488e0fa01f320aa286a
Guilherme Dias <guilhermehenriquedias@hotmail.com>
2021-10-04 07:43:07 -0300

    adding repo page

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon
+} from "./styles";
 
 const Repo: React.FC = () => {
-    return <div />;
-}
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={"username"} to="/guihendias">
+          guihendias
+        </Link>
+
+        <span>/</span>
+
+        <Link className={"reponame"} to="/guihendias/spotify-clone">
+          spotify-clone
+        </Link>
+      </Breadcrumb>
+
+      <p>Descrição teste do repositório</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href="http://github.com/guihendias/spotify-clone">
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  );
+};
 
 export default Repo;
